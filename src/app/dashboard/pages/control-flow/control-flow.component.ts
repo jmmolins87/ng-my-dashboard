098998dfefcb77a0f27d1ec89803525e5884d554
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-04-17 12:11:39 +0200

    wip

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A' | 'B' | 'C' | 'D' | 'E';
 
 @Component({
   standalone: true,
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './control-flow.component.html',
   styles: ``
 })
-export class ControlFlowComponent {
+export default class ControlFlowComponent {
 
+  public showContent = signal( false );
+  public grade = signal<Grade>('E');
+  public frameworks = signal(['Angular', 'React', 'Vue', 'Svelte', 'Qwik']);
+  public frameworksNew = signal([]);
+  
+  public toggleContent() {
+    this.showContent.update( value => !value );
+  }
 }
--- src/app/dashboard/shared/sidemenu/sidemenu.component.ts
+++ src/app/dashboard/shared/sidemenu/sidemenu.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+    .map( route => route.children ?? [])
+    .flat()
+    .filter( route => route && route.path )
+    .filter( route => !route.path?.includes(':'))
+
+  constructor() { }
 }
