c46c8d7b080aa23d47e92fbbdf3008ed341bce16
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-04-17 10:05:20 +0200

    fix scafolder

---
+++ src/app/dashboard/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [CommonModule],
+  template: ``
+})
+export class UsersLoaderComponent {
+
+}
---
+++ src/app/dashboard/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidemenu.component.html',
+  styles: ``
+})
+export class SidemenuComponent {
+
+}
